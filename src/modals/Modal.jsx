2f94b2193fe066bb9cca03e2b1b0fcca82f3fe48
Vasco Ceti <vasco.ceti@gmail.com>
2024-03-04 15:38:48 +0100

    toast fixed in modal

--- src/modals/Modal.jsx
+++ src/modals/Modal.jsx
@@ -3,18 +3,39 @@ import { createPortal } from "react-dom";
 
 function Modal({ children, className = "modal", isOpen, onClose }) {
   const modalref = useRef(null);
-  useEffect(() => {
-    if (isOpen) {
-      modalref.current.showModal();
-    } else {
-      modalref.current.close();
-    }
-  }, [isOpen]);
+  // useEffect(() => {
+  //   if (isOpen) {
+  //     modalref.current.showModal();
+  //   } else {
+  //     modalref.current.close();
+  //   }
+  // }, [isOpen]);
+
+  const display = isOpen ? "flex" : "none";
+  const ModalStyle = {
+    display: display,
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.75)",
+    zIndex: "10000",
+    justifyContent: "center",
+    alignItems: "center",
+  };
 
   return createPortal(
-    <dialog ref={modalref} className={className} onClose={onClose}>
-      {children}
-    </dialog>,
+    <div style={ModalStyle}>
+      <dialog
+        ref={modalref}
+        open={isOpen}
+        className={className}
+        onClose={onClose}
+      >
+        {children}
+      </dialog>
+    </div>,
     document.getElementById("modal")
   );
 }
